14c1f457e33659ba48fbd6ee41605539e4b76c90
TheKingKerellos <101502846+TheKingKerellos@users.noreply.github.com>
2024-09-19 16:56:46 +0100

    Update glibc.sh to lfs 12.2

--- phase1/glibc.sh
+++ phase1/glibc.sh
@@ -1,11 +1,9 @@
 # Glibc Phase 1
 case $(uname -m) in
-    i?86)
-        ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+    i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
     ;;
-    x86_64)
-        ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
-        ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
     ;;
 esac
 
@@ -20,14 +18,13 @@ echo "rootsbindir=/usr/sbin" > configparms
       --prefix=/usr                      \
       --host=$LFS_TGT                    \
       --build=$(../scripts/config.guess) \
-      --enable-kernel=3.2                \
+      --enable-kernel=4.19               \
       --with-headers=$LFS/usr/include    \
+      --disable-nscd                     \
       libc_cv_slibdir=/usr/lib
 
 make
 make DESTDIR=$LFS install
 
 sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
-
-$LFS/tools/libexec/gcc/$LFS_TGT/12.2.0/install-tools/mkheaders
 
