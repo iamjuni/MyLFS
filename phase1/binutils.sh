7ae1296c0b289ea4ffc5c036eb0babbb7b80c9f0
TheKingKerellos <101502846+TheKingKerellos@users.noreply.github.com>
2024-09-19 15:38:41 +0100

    Update binutils.sh for lfs 12.2

--- phase1/binutils.sh
+++ phase1/binutils.sh
@@ -3,13 +3,14 @@
 mkdir build
 cd build
 
-../configure \
-    --prefix=$LFS/tools \
-    --with-sysroot=$LFS \
-    --target=$LFS_TGT \
-    --disable-nls \
-    --enable-gprofng=no \
-    --disable-werror
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror    \
+             --enable-new-dtags  \
+             --enable-default-hash-style=gnu
 
 make
 
