3333a2a8998eb2f42693b32c49ac6edd5d287593
TheKingKerellos <101502846+TheKingKerellos@users.noreply.github.com>
2024-09-24 20:09:29 +0100

    Update readline.sh to lfs 12.2

--- phase4/readline.sh
+++ phase4/readline.sh
@@ -1,14 +1,13 @@
 # Readline Phase 4
-sed -i '/MV.*old/d' Makefile.in
-sed -i '/{OLDSUFF}/c:' support/shlib-install
+./configure --prefix=/usr
 
-./configure --prefix=/usr    \
-            --disable-static \
-            --with-curses    \
-            --docdir=/usr/share/doc/readline-8.1.2
+make
 
-make SHLIB_LIBS="-lncursesw"
-make SHLIB_LIBS="-lncursesw" install
+if $RUN_TESTS
+then
+    set +e
+    make check
+    set -e
+fi
 
-install -m644 doc/*.{ps,pdf,html,dvi} /usr/share/doc/readline-8.1.2
-
+make install
