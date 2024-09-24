12aa463b8c7b1eb47155df7b3f681f0d97ad5545
TheKingKerellos <101502846+TheKingKerellos@users.noreply.github.com>
2024-09-24 19:53:00 +0100

    Update mandb.sh to lfs 12.2

--- phase4/mandb.sh
+++ phase4/mandb.sh
@@ -1,16 +1,7 @@
 # Man-DB Phase 4
-./configure --prefix=/usr                         \
-            --docdir=/usr/share/doc/man-db-2.10.2 \
-            --sysconfdir=/etc                     \
-            --disable-setuid                      \
-            --enable-cache-owner=bin              \
-            --with-browser=/usr/bin/lynx          \
-            --with-vgrind=/usr/bin/vgrind         \
-            --with-grap=/usr/bin/grap             \
-            --with-systemdtmpfilesdir=            \
-            --with-systemdsystemunitdir=
+rm  man3/crypt*
 
-make
+make prefix=/usr install
 
 if $RUN_TESTS
 then
