0ec7ca31ac183eeebdd3a2e04e320e6f3fa31cda
iamjuni <116604152+iamjuni@users.noreply.github.com>
2025-03-02 17:08:11 -0800

    Add Apache

--- example_extension/packages.sh
+++ example_extension/packages.sh
@@ -6,4 +6,4 @@ export PKG_LIBTASN1=https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.19.0.tar.gz
 export PKG_P11KIT=https://github.com/p11-glue/p11-kit/releases/download/0.25.5/p11-kit-0.25.5.tar.xz
 export PKG_MAKECA=https://github.com/lfs-book/make-ca/archive/v1.14/make-ca-1.14.tar.gz
 export PKG_CURL=https://curl.se/download/curl-8.9.1.tar.xz
-
+export PKG_APACHE=https://archive.apache.org/dist/httpd/httpd-2.4.62.tar.bz2
